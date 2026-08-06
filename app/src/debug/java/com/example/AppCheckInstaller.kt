efba609a65c3add7fd858e28cac3752d9e375683
vhanukaev1981 <64756523+vhanukaev1981@users.noreply.github.com>
2026-08-06 20:50:52 +0300

    isolate debug App Check provider

---
+++ app/src/debug/java/com/example/AppCheckInstaller.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+
+object AppCheckInstaller {
+    fun install() {
+        FirebaseAppCheck.getInstance().installAppCheckProviderFactory(
+            DebugAppCheckProviderFactory.getInstance(),
+            true
+        )
+    }
+}
