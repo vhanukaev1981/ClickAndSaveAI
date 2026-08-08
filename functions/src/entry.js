2ca3c11b4a38d94289c107cae6cc6a65c0c764cf
vhanukaev1981 <64756523+vhanukaev1981@users.noreply.github.com>
2026-08-08 19:49:31 +0300

    Export proactive financial agent function

--- functions/src/entry.js
+++ functions/src/entry.js
@@ -4,10 +4,12 @@ const coreFunctions = require("./index");
 const pushFunctions = require("./pushFunctions");
 const gmailWatchFunctions = require("./gmailWatchFunctions");
 const gmailWatchRenewal = require("./gmailWatchRenewal");
+const financialAgentFunctions = require("./financialAgentFunctions");
 
 module.exports = {
   ...coreFunctions,
   ...pushFunctions,
   ...gmailWatchFunctions,
   ...gmailWatchRenewal,
+  ...financialAgentFunctions,
 };
