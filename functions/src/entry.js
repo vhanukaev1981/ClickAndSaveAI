7bb41c524241bd88f9b1b6062cb4654742dd54d8
vhanukaev1981 <64756523+vhanukaev1981@users.noreply.github.com>
2026-08-08 16:18:26 +0300

    Expose scheduled Gmail watch renewal

--- functions/src/entry.js
+++ functions/src/entry.js
@@ -3,9 +3,11 @@
 const coreFunctions = require("./index");
 const pushFunctions = require("./pushFunctions");
 const gmailWatchFunctions = require("./gmailWatchFunctions");
+const gmailWatchRenewal = require("./gmailWatchRenewal");
 
 module.exports = {
   ...coreFunctions,
   ...pushFunctions,
   ...gmailWatchFunctions,
+  ...gmailWatchRenewal,
 };
