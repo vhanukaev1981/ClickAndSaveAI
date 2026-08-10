1ee2cfd2113bc3f3417dc9381cee98af148d27f9
vhanukaev1981 <64756523+vhanukaev1981@users.noreply.github.com>
2026-08-10 06:30:50 +0300

    Test exact push navigation target first

--- app/src/test/java/com/example/PushNavigationPolicyTest.kt
+++ app/src/test/java/com/example/PushNavigationPolicyTest.kt
@@ -8,16 +8,61 @@ class PushNavigationPolicyTest {
     @Test
     fun newInvoiceOpensBills() {
         assertEquals(1, destinationTabForPushType(PUSH_TYPE_NEW_INVOICE))
+        assertEquals(
+            PushNavigationTarget(tab = 1),
+            navigationTargetForPush(PUSH_TYPE_NEW_INVOICE, null, null)
+        )
     }
 
     @Test
-    fun verifiedSavingsOpensSavings() {
+    fun verifiedSavingsRequiresAndPreservesExactOpportunityAndOffer() {
         assertEquals(2, destinationTabForPushType(PUSH_TYPE_VERIFIED_SAVINGS_OPPORTUNITY))
+        assertEquals(
+            PushNavigationTarget(
+                tab = 2,
+                opportunityId = "opp-1",
+                offerId = "offer-1"
+            ),
+            navigationTargetForPush(
+                PUSH_TYPE_VERIFIED_SAVINGS_OPPORTUNITY,
+                "opp-1",
+                "offer-1"
+            )
+        )
+    }
+
+    @Test
+    fun verifiedSavingsWithoutExactIdsCannotNavigate() {
+        assertNull(
+            navigationTargetForPush(
+                PUSH_TYPE_VERIFIED_SAVINGS_OPPORTUNITY,
+                "",
+                "offer-1"
+            )
+        )
+        assertNull(
+            navigationTargetForPush(
+                PUSH_TYPE_VERIFIED_SAVINGS_OPPORTUNITY,
+                "opp-1",
+                ""
+            )
+        )
+        assertNull(
+            navigationTargetForPush(
+                PUSH_TYPE_VERIFIED_SAVINGS_OPPORTUNITY,
+                null,
+                null
+            )
+        )
     }
 
     @Test
     fun testPushOpensDashboard() {
         assertEquals(0, destinationTabForPushType(PUSH_TYPE_TEST))
+        assertEquals(
+            PushNavigationTarget(tab = 0),
+            navigationTargetForPush(PUSH_TYPE_TEST, null, null)
+        )
     }
 
     @Test
@@ -25,5 +70,8 @@ class PushNavigationPolicyTest {
         assertNull(destinationTabForPushType("OPEN_PROFILE"))
         assertNull(destinationTabForPushType("99"))
         assertNull(destinationTabForPushType(null))
+        assertNull(navigationTargetForPush("OPEN_PROFILE", "opp-1", "offer-1"))
+        assertNull(navigationTargetForPush("99", "opp-1", "offer-1"))
+        assertNull(navigationTargetForPush(null, "opp-1", "offer-1"))
     }
 }
